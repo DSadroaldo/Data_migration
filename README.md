458a0487f80b86dc32dd06be2218d0511347cda4
DSAdro <infotecnasp@gmail.com>
2025-05-28 22:15:12 -0300

    Commit_create_router_user_BD

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ git remote set-url origin https://github.com/DSadroaldo/Data_migration.git // > 
 <!-- ![doc_autorizacao_usuario](docs_API_auth.png) -->
 
 ![rota_pedidos](docs_rota_pedido.png)
+
+<!-- criacao da conta do usuario
+  como estruturar o processo de crição de itens no seu banco de dados
+  garantir sessoes no banco de dados maneira robusta e escalavel
+  atender a fechar sessoes, sem deixa-las abertas
+  A base do processo de criação rotas, endpoints, links-->
+
+<!--
+  # sqlalchemy, ORM(obj) - > permite que você interaja com o seu banco de dados usando objetos Python em vez de escrever comandos SQL diretamente. Ele "mapeia" as classes Python para tabelas do banco de dados e os objetos (instâncias dessas classes) para as linhas dessas tabelas. -->
+
+<!-- Sessions Restrições
+ e importante gerenciar as sessoes de conexões no banco de dados
+  isto é, torna-se impreenscídivel a finalização das conexões abertas no banco de dados
+  CRIAR | FAZER o q TEM FAZER | FECHAR -->
+
+  <!-- Criamos primeiro escopo de rota de criação e autenticação de usuário
+  sem nos atermos a regras mais seguras. Aqui nos atemos a gerar a teste de rota e create de um item 
+  a nível de teste  -->
--- auth_routes.py
+++ auth_routes.py
@@ -1,16 +1,31 @@
 from fastapi import APIRouter
- 
+from models import Usuario, db
+from sqlalchemy.orm import sessionmaker
+
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def autenticar():
+async def home():
   '''
   Essa é a rota de autenticação de pedidos do sistema, criado por Adroaldo!
   '''
   return{"mensagem": "voce acessou a rota padrão de autenticação", "autenticado": False}
 
+# criarmos uma rota de criar um user (criamos um item dentro do nosso db)
+# toda informação que nos enviarmos p/nossa rota (post) será recebida pela nossa função async
+@auth_router.post("/criar_conta") # postando informações nela e com elas estou editando dentr do sistema  
+async def criar_conta(nome: str,email: str, senha: str): # -> na estrutura do FastAPi eh necessario a tipagem de dados
+    Session = sessionmaker(bind=db) # -> aqui estou criando uma conexão com meu BD
+    session = Session() # -> instanciando esta sessão 
+    usuario = session.query(Usuario).filter(Usuario.email==email).first() # isso nos dara uma query no Banco
+    if usuario:
+       return{"mensagem": "Ja existe um usuário com este email"}
+    else:
+       novo_usuario = Usuario(nome, email, senha)
+       session.add(novo_usuario)
+       session.commit()
+       return{"mensagem": "Usuário cadastrado com sucesso"}
+    
 
-  
 
 
-  # sqlalchemy, ORM(obj) - > permite que você interaja com o seu banco de dados usando objetos Python em vez de escrever comandos SQL diretamente. Ele "mapeia" as classes Python para tabelas do banco de dados e os objetos (instâncias dessas classes) para as linhas dessas tabelas.
